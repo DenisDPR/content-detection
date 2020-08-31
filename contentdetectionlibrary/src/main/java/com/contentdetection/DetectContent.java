36aef85ea76898b125a04b9c21bb90a4daba97db
shebogholo <shebogholo@gmail.com>
2020-08-31 07:27:18 +0300

    Change method from s to detect

--- contentdetectionlibrary/src/main/java/com/contentdetection/DetectContent.java
+++ contentdetectionlibrary/src/main/java/com/contentdetection/DetectContent.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class DetectContent {
-    public static void s(Context context, String text){
+    public static void detect(Context context, String text){
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
     }
 }
