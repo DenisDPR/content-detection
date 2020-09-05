e970f08db79375f67d212c6e8743a6ead9845263
shebogholo <shebogholo@gmail.com>
2020-09-05 10:20:46 +0300

    added threshold

--- library/src/main/java/com/shebogholo/library/DetectContent.java
+++ library/src/main/java/com/shebogholo/library/DetectContent.java
@@ -37,8 +37,9 @@ public class DetectContent {
         textClassificationClient.load();
         List<DetectContent.TextClassificationClient.Result> results = textClassificationClient.classify(text);
         DetectContent.TextClassificationClient.Result result = results.get(1);
+        System.out.println(result.confidence);
         textClassificationClient.unload();
-        if (result.confidence > 0.40){
+        if (result.confidence > 0.53){
             return "unsuccessful";
         }else{
             return text;
